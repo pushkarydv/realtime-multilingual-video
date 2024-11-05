49924318f95800bad0d57e09c6a4d216e3ab4742
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2024-11-05 17:43:51 +0530

    IPC based button  + Style Fixes

--- renderer.js
+++ renderer.js
@@ -1,52 +1,5 @@
-// const filePicker = document.getElementById("filePicker");
-// const videoPlayer = document.getElementById("videoPlayer");
+const videoPlayer = document.getElementById("videoPlayer");
 
-// filePicker.addEventListener("change", (event) => {
-//   const file = event.target.files[0];
-//   if (file) {
-//     const videoUrl = URL.createObjectURL(file);
-//     console.log(file.path);
-
-//     videoPlayer.style.display = "block";
-//     videoPlayer.src = videoUrl;
-//   }
-// });
-
-// document.addEventListener("dragover", (e) => {
-//   e.preventDefault();
-//   e.stopPropagation();
-// });
-
-// document.addEventListener("drop", (e) => {
-//   e.preventDefault();
-//   e.stopPropagation();
-
-//   const file = e.dataTransfer.files[0];
-//   if (file && file.type.startsWith("video/")) {
-//     const videoUrl = URL.createObjectURL(file);
-//     console.log(file);
-//     videoPlayer.style.display = "block";
-//     videoPlayer.src = videoUrl;
-//     document.getElementById("placeholder").style.display = "none";
-//   }
-// });
-
-// videoPlayer.addEventListener("loadeddata", () => {
-//   document.getElementById("placeholder").style.display = "none";
-// });
-
-// filePicker.addEventListener("click", (e) => {
-//   e.stopPropagation();
-// });
-
-// document.getElementById("placeholder").addEventListener("click", () => {
-//   filePicker.click();
-// });
-
-// videoPlayer.addEventListener("error", () => {
-//   videoPlayer.style.display = "none";
-//   document.getElementById("placeholder").style.display = "flex";
-// });
 
 // Custom Method to extract audio from video
 // async function extractAudioFromVideoFile(file) {
@@ -147,10 +100,13 @@
 //     });
 // });
 
-const selectFileutton = document.getElementById("select-file-button");
-const filePathElement = document.getElementById("filePath");
+const selectFileButton = document.getElementById("select-file-button");
 
-selectFileutton.addEventListener("click", async () => {
+selectFileButton.addEventListener("click", async () => {
   const filePath = await window.electron.openFile();
-  filePathElement.innerText = filePath;
+  if (filePath) {
+    selectFileButton.style.display = "none";
+    videoPlayer.style.display = "block";
+    videoPlayer.src = filePath;
+  }
 });
